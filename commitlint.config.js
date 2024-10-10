07a76391e2dadd07a539039000eaab2ec567298e
hnochx <hnochx@gmail.com>
2024-10-10 12:59:43 +0900

    Feat: eslint + prettier + commitlint

---
+++ commitlint.config.js
@@ -0,0 +1,29 @@
+export default {
+  extends: ['@commitlint/config-conventional'],
+
+  parserPreset: {
+    parserOpts: {
+      headerPattern: '^(?<type>.+):\\s(?<subject>.+)$',
+
+      headerCorrespondence: ['type', 'subject'],
+    },
+  },
+
+  rules: {
+    'subject-empty': [2, 'never'],
+
+    'subject-full-stop': [2, 'never', '.'],
+
+    'type-case': [0],
+
+    'type-empty': [2, 'never'],
+
+    'type-enum': [
+      2,
+
+      'always',
+
+      ['Init', 'Feat', 'Chore', 'Design', 'Fix', 'Test', 'Deploy', 'Refactor', 'Rename', 'Remove', 'Comment'],
+    ],
+  },
+};
