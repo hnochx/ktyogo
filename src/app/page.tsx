b61aadd61ee7dd3eedb48ec6f11eb4c59b89557f
SH1NW00-0H <dottccang523@gmail.com>
2024-10-11 19:02:53 +0900

    Feat: footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+import Footer from '../components/footer';
+
 export default function Home() {
-  return <div></div>;
+  return (
+    <>
+      <Footer />
+    </>
+  );
 }
---
+++ src/components/footer.tsx
@@ -0,0 +1,22 @@
+export default function footer() {
+  return (
+    <>
+      <div className="w-full h-[310px] bg-[#f2f2f2] py-8 px-5">
+        <h1 className="text-sm font-bold text-[#666666] mb-4">KT 공식 온라인샵</h1>
+        <p className="text-xs font-light text-[#666666] mb-4">
+          (주) 케이티 대표이사 김영섭 사업자등록번호 :102-81-42945 통신판매업신고 : 2002-경기성남-0048 사업자정보확인
+          경기도 성남시 분당구 불정로 90 (정자동) <br />
+          <span className={'font-bold'}>[휴대폰, 인터넷/TV 가입문의] : 1588-8001(유료) 080-515-9000 (무료) </span>
+          (점심시간 휴식 12시~13시)
+        </p>
+        <p className="text-xs font-light text-[#666666]">
+          휴대폰 상담 평일 09시~18시, 토요일 09시~12시(내선번호 1번) | 인터넷/TV 상담 평일 09시~18시 (내선번호 2번) |
+          액세서리상담 평일 09시~18시 (내선번호 3번) [5시 핫딜 응모/당첨/배송 문의] : 080-515-9004 고객센터 : [모바일]
+          휴대폰+114(무료), 1588-0010(유료) [인터넷/TV/전화] 유선 전화 (국번없이) 100, 휴대폰(지역번호)+100,
+          1514(발신전용), 1524(발신전용) <br />
+          Copyrightⓒ kt corp. All rights reserved.
+        </p>
+      </div>
+    </>
+  );
+}
