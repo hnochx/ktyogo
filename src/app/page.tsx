8d884c4978e8e6332063d587afaeb54f6cd2138a
jio <jiographie@gmail.com>
2024-10-14 10:15:38 +0900

    Chore: page.tsx 초기화 Ref:#2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,3 @@
-import Header from '@/components/header/header';
-import SubHeader from '@/components/header/sub-header';
-
 export default function Home() {
-  return (
-    <div>
-      <SubHeader />
-      <Header />
-      home range home
-    </div>
-  );
+  return <div></div>;
 }
