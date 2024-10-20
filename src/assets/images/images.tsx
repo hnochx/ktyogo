31cf95b861be07e18e2522f16822ccc883c5a36e
hnochx <hnochx@gmail.com>
2024-10-20 21:58:27 +0900

    Chore: remove img Ref:#21

--- src/assets/images/images.tsx
+++ src/assets/images/images.tsx
@@ -1,5 +1,4 @@
 import kt_logo from './kt_logo.svg';
 import icon_send from './chatbot/icon_send.png';
-import banner_yogo from './chatbot/banner_yogo.png';
 
-export { kt_logo, icon_send, banner_yogo };
+export { kt_logo, icon_send };
