160550db67156414c7cde657979e824af44d1e01
hnochx <hnochx@gmail.com>
2024-10-23 14:16:00 +0900

    Feat: global font & chatbot link Ref:#21

--- src/app/(Layout)/layout.tsx
+++ src/app/(Layout)/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import '../globals.css';
 import Footer from '@/components/footer/Footer';
 import HeaderBox from '@/components/header/HeaderBox';
+import Link from 'next/link';
 
 export const metadata: Metadata = {
   title: 'KT',
@@ -17,7 +18,15 @@ export default function RootLayout({
     <html lang="ko">
       <body>
         <HeaderBox />
-        <div className="min-h-[100vh] pt-[112px]">{children}</div>
+        <div className="min-h-[100vh] pt-[112px]">
+          {children}
+          <Link
+            href={'/chatbot'}
+            className="fixed bottom-[5%] right-[5%] w-[3rem] h-[3rem] rounded-full flex items-center justify-center border-[3px] border-red-400 bg-white z-[1]"
+          >
+            <div className=" font-bold text-black text-[0.95rem]">챗봇</div>
+          </Link>
+        </div>
         <Footer />
       </body>
     </html>
